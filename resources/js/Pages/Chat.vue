cede38d5994252011a9689dd3638778fa79568ed
pedroleandro <pedro.leandrog@gmail.com>
2022-03-01 20:07:12 -0300

    Listagem de usuários

--- resources/js/Pages/Chat.vue
+++ resources/js/Pages/Chat.vue
@@ -8,55 +8,15 @@
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg flex" style="min-height: 400px; max-height: 400px;">
+                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg flex"
+                     style="min-height: 400px; max-height: 400px;">
                     <!--                    Listagem de usuários-->
                     <div class="w-3/12 bg-gray-200 bg-opacity-25 border-r border-gray-200 overflow-y-scroll">
                         <ul>
-                            <li class="p-6 text-lg text-gray-600 leading-7 font-semibold border-b border-gray-200 hover:bg-gray-200 hover:bg-opacity-50 hover:cursor-pointer">
+                            <li v-for="user in users" :key="user.id"
+                                class="p-6 text-lg text-gray-600 leading-7 font-semibold border-b border-gray-200 hover:bg-gray-200 hover:bg-opacity-50 hover:cursor-pointer">
                                 <p class="flex items-center">
-                                    Tiago Pereira
-                                    <span class="ml-2 w-2 h-2 bg-blue-500 rounded-full"></span>
-                                </p>
-                            </li>
-                            <li class="p-6 text-lg text-gray-600 leading-7 font-semibold border-b border-gray-200 hover:bg-gray-200 hover:bg-opacity-50 hover:cursor-pointer">
-                                <p class="flex items-center">
-                                    Thiago da Cruz
-                                    <span class="ml-2 w-2 h-2 bg-blue-500 rounded-full"></span>
-                                </p>
-                            </li>
-                            <li class="p-6 text-lg text-gray-600 leading-7 font-semibold border-b border-gray-200 hover:bg-gray-200 hover:bg-opacity-50 hover:cursor-pointer">
-                                <p class="flex items-center">
-                                    Gerson James
-                                    <span class="ml-2 w-2 h-2 bg-blue-500 rounded-full"></span>
-                                </p>
-                            </li>
-                            <li class="p-6 text-lg text-gray-600 leading-7 font-semibold border-b border-gray-200 hover:bg-gray-200 hover:bg-opacity-50 hover:cursor-pointer">
-                                <p class="flex items-center">
-                                    Juan Carlos
-                                    <span class="ml-2 w-2 h-2 bg-blue-500 rounded-full"></span>
-                                </p>
-                            </li>
-                            <li class="p-6 text-lg text-gray-600 leading-7 font-semibold border-b border-gray-200 hover:bg-gray-200 hover:bg-opacity-50 hover:cursor-pointer">
-                                <p class="flex items-center">
-                                    Airton Sousa
-                                    <span class="ml-2 w-2 h-2 bg-blue-500 rounded-full"></span>
-                                </p>
-                            </li>
-                            <li class="p-6 text-lg text-gray-600 leading-7 font-semibold border-b border-gray-200 hover:bg-gray-200 hover:bg-opacity-50 hover:cursor-pointer">
-                                <p class="flex items-center">
-                                    João Pedro
-                                    <span class="ml-2 w-2 h-2 bg-blue-500 rounded-full"></span>
-                                </p>
-                            </li>
-                            <li class="p-6 text-lg text-gray-600 leading-7 font-semibold border-b border-gray-200 hover:bg-gray-200 hover:bg-opacity-50 hover:cursor-pointer">
-                                <p class="flex items-center">
-                                    Helano Miguel
-                                    <span class="ml-2 w-2 h-2 bg-blue-500 rounded-full"></span>
-                                </p>
-                            </li>
-                            <li class="p-6 text-lg text-gray-600 leading-7 font-semibold border-b border-gray-200 hover:bg-gray-200 hover:bg-opacity-50 hover:cursor-pointer">
-                                <p class="flex items-center">
-                                    Luis Fernando Maia
+                                    {{ user.name }}
                                     <span class="ml-2 w-2 h-2 bg-blue-500 rounded-full"></span>
                                 </p>
                             </li>
@@ -184,7 +144,8 @@
                             <form action="">
                                 <div class="flex rounded-md overflow-hidden border border-gray-300">
                                     <input type="text" class="flex-1 px-4 py-2 text-sm focus:outline-none">
-                                    <button type="submit" class="ml-0.5 bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2">
+                                    <button type="submit"
+                                            class="ml-0.5 bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2">
                                         Enviar
                                     </button>
                                 </div>
@@ -205,5 +166,15 @@ export default defineComponent({
     components: {
         AppLayout,
     },
+    data() {
+        return {
+            users: []
+        }
+    },
+    mounted() {
+        axios.get('/api/users').then(response => {
+            this.users = response.data.users
+        });
+    }
 })
 </script>
