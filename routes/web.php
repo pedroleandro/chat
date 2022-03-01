2864079cae08fc362374a1603d2feb5ae3d2e227
pedroleandro <pedro.leandrog@gmail.com>
2022-03-01 02:43:37 -0300

    Criação do componente Chat

--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,6 @@ Route::get('/', [PageController::class, 'welcome'])->name('welcome');
 Route::group(['middleware' => ['auth:sanctum', 'verified']], function () {
 
     Route::get('/dashboard', [PageController::class, 'dashboard'])->name('dashboard');
+    Route::get('/chat', [PageController::class, 'chat'])->name('chat');
 
 });
