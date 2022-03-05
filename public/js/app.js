5c7e93a3017174d1f294c10fd4c998186af8146e
pedroleandro <pedro.leandrog@gmail.com>
2022-03-05 10:07:37 -0300

    feat(chat): notificações de mensagens não lidas
    Foi habilitado a notificação de mensagens ainda não lidas na conversa pelo chat.

--- public/js/app.js
+++ public/js/app.js
@@ -20770,6 +20770,7 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
       var _loadMessages = _asyncToGenerator( /*#__PURE__*/_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default().mark(function _callee(userId) {
         var _this = this;
 
+        var user;
         return _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default().wrap(function _callee$(_context) {
           while (1) {
             switch (_context.prev = _context.next) {
@@ -20783,9 +20784,19 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
                 });
 
               case 3:
+                user = this.users.filter(function (user) {
+                  if (user.id === userId) {
+                    return user;
+                  }
+                });
+
+                if (user) {
+                  user[0].notification = false;
+                }
+
                 this.scrollToBottom();
 
-              case 4:
+              case 6:
               case "end":
                 return _context.stop();
             }
@@ -20853,6 +20864,7 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
     });
     Echo["private"]("user.".concat(this.user.id)).listen('.SendMessage', /*#__PURE__*/function () {
       var _ref = _asyncToGenerator( /*#__PURE__*/_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default().mark(function _callee3(event) {
+        var user;
         return _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default().wrap(function _callee3$(_context3) {
           while (1) {
             switch (_context3.prev = _context3.next) {
@@ -20868,10 +20880,21 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
               case 3:
                 _this3.scrollToBottom();
 
-                _context3.next = 6;
+                _context3.next = 8;
                 break;
 
               case 6:
+                user = _this3.users.filter(function (user) {
+                  if (user.id === event.message.from) {
+                    return user;
+                  }
+                });
+
+                if (user) {
+                  user[0].notification = true;
+                }
+
+              case 8:
               case "end":
                 return _context3.stop();
             }
@@ -24719,13 +24742,10 @@ var _hoisted_6 = ["onClick"];
 var _hoisted_7 = {
   "class": "flex items-center"
 };
-
-var _hoisted_8 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("span", {
-  "class": "ml-2 w-2 h-2 bg-blue-500 rounded-full"
-}, null, -1
-/* HOISTED */
-);
-
+var _hoisted_8 = {
+  key: 0,
+  "class": "ml-2 w-2 h-2 bg-red-500 rounded-full disabled"
+};
 var _hoisted_9 = {
   "class": "w-9/12 flex flex-col justify-between"
 };
@@ -24769,7 +24789,7 @@ function render(_ctx, _cache, $props, $setup, $data, $options) {
           "class": (0,vue__WEBPACK_IMPORTED_MODULE_0__.normalizeClass)([_ctx.userActive && _ctx.userActive.id == user.id ? 'bg-blue-400 bg-opacity-50' : '', "p-6 text-lg text-gray-600 leading-7 font-semibold border-b border-gray-200 hover:bg-blue-400 hover:bg-opacity-50 hover:cursor-pointer"])
         }, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("p", _hoisted_7, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createTextVNode)((0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)(user.name) + " ", 1
         /* TEXT */
-        ), _hoisted_8])], 10
+        ), user.notification ? ((0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("span", _hoisted_8)) : (0,vue__WEBPACK_IMPORTED_MODULE_0__.createCommentVNode)("v-if", true)])], 10
         /* CLASS, PROPS */
         , _hoisted_6);
       }), 128
